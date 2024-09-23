800731ec63689d7ccd6c0978f2b46b4c7b0afe16
BoByUkn <paolo.grilly@viacesi.fr>
2024-09-23 14:29:36 -0400

    fix: file handle

--- app/version/filehandle.py
+++ app/version/filehandle.py
@@ -1,30 +1,35 @@
 import os
 
-EXE_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app', 'exe_files', '1.0.0'))
+# Mettre à jour le chemin de EXE_FOLDER pour pointer vers le bon répertoire
+EXE_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'exe_files'))
 
-print(f"Répertoire EXE_FOLDER configuré sur : {EXE_FOLDER}")
 
 def get_latest_version():
     exe_files = []
+    print(f"Répertoire EXE_FOLDER configuré sur : {EXE_FOLDER}")
+
+    # Parcourir les répertoires pour rechercher les fichiers .exe
     for root, dirs, files in os.walk(EXE_FOLDER):
-        # Filtrer uniquement les répertoires de version (par exemple, "1.0.0")
-        if root != EXE_FOLDER:
-            for file in files:
-                if file.endswith('.exe'):
-                    full_path = os.path.join(root, file)
-                    exe_files.append(full_path)
-                    print(f"Fichier .exe trouvé : {full_path}")
+        for file in files:
+            if file.endswith('.exe'):
+                full_path = os.path.join(root, file)
+                exe_files.append(full_path)
+                print(f"Fichier .exe trouvé : {full_path}")
 
     if not exe_files:
         print("Aucun fichier .exe trouvé")
         return None
 
     def version_key(file_path):
-        file_name = os.path.basename(file_path)
-        # Modification de cette ligne pour extraire correctement la version
+        # Extraire la version à partir du chemin du répertoire parent
         version_str = os.path.basename(os.path.dirname(file_path))
-        return tuple(map(int, version_str.split('.')))
+        try:
+            return tuple(map(int, version_str.split('.')))
+        except ValueError:
+            # Si la version ne peut pas être convertie en tuple d'entiers, retournez un tuple vide (0, 0, 0)
+            return (0, 0, 0)
 
+    # Trouver le fichier avec la version la plus récente
     latest_file = max(exe_files, key=version_key)
     print(f"Le fichier .exe le plus récent est : {latest_file}")
     return latest_file
